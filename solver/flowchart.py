3ca3a17ee5da8d10264e0fd762268be4df66eb95
Cameron Sun <cameron.csun@gmail.com>
2025-05-16 18:25:56 -0700

    Add flowchart generator

--- solver/flowchart.py
+++ solver/flowchart.py
@@ -5,84 +5,93 @@ import json
 class FlowNode:
     def __init__(self, word):
         self.word = word
-        self.child_counts = {}
-        self.siblings = set()
-        self.relevant_siblings = []
+        self.prev_counts = {}
         self.occurrences = 0
-        
-    def register_path(self, optimal_path):
-        self.occurrences += 1
-        this_len = len(self.word)
-        for path_word in optimal_path:
-            if len(path_word) == this_len - 1:
-                self.child_counts[path_word] = self.child_counts.get(path_word, 0) + 1
-            if len(path_word) == this_len:
-                self.siblings.add(path_word)
+    
+    def prev_and_others(self, occurrence_threshold):
+        prev = [word for word, count in sorted(self.prev_counts.items(), key=lambda x: x[1], reverse=True)
+                if count >= occurrence_threshold]
 
-relationships_nodes = json.load(open('graph.json'))
-true_parents = {}
-for node in relationships_nodes:
-    true_parents[node['word']] = [relationships_nodes[child]['word'] for child in node['children']]
+        return prev[:2], [word for word, _ in self.prev_counts.items() if word not in prev[:2]]
 
 nodes = json.load(open('optimums.json'))
 
 flow_nodes = {}
 for node in nodes:
     optimal_path = node['optimal_path']
+    prev = None
     for word in optimal_path:
         if word not in flow_nodes:
             flow_nodes[word] = FlowNode(word)
-        flow_nodes[word].register_path(optimal_path)
+        flow_nodes[word].occurrences += 1
+
+        if prev is not None:
+            flow_nodes[word].prev_counts[prev] = flow_nodes[word].prev_counts.get(prev, 0) + 1
+        prev = word
 
 graph = graphviz.Digraph('Move Flowchart') 
-explore = queue.Queue()
-explore.put('FORMALITIES')
-explore.put('PHRENOLOGIES')
 
-visited = set()
-while not explore.empty():
-    word = explore.get()
-    if word in visited:
-        continue
+def graph_roots(roots, occurrence_threshold):
+    explore = queue.Queue()
+    for root in roots:
+        explore.put(root)
 
-    visited.add(word)
-    node = flow_nodes[word]
-    true_children = [(child, count) for child, count in node.child_counts.items if node.word in true_parents[child]]
-    sorted_children = sorted(true_children, key=lambda x: x[1])
-
-    # Combine so that we max out at 2 children and "other"
-    filtered_children = []
-    other_count = 0
-    while len(sorted_children) > 0:
-        child_word, child_count = sorted_children.pop()
-        candidate = flow_nodes[child_word]
-
-        if child_count < 40 or len(filtered_children) >= 2:
-            other_count += 1
+    cached_others = {}
+    cached_sorted_prev_count = {}
+    visited = set()
+    while not explore.empty():
+        word = explore.get()
+        if word in visited:
             continue
 
-        relevant_siblings = []
-        for sibling_word, sibling_count in sorted_children:
-            if sibling_word in candidate.siblings:
-                candidate.relevant_siblings.append(sibling_word)
-                relevant_siblings.append((sibling_word, sibling_count))
-                
-        for sibling in relevant_siblings:
-            sorted_children.remove(sibling)
+        visited.add(word)
+        node = flow_nodes[word]
+
+        if node.occurrences >= 1000:
+            fontsize = '20'
+        elif node.occurrences >= 300:
+            fontsize = '16'
+        else:
+            fontsize = '12'
+
+        graph.node(
+            word,
+            label=f'{word}\n{node.occurrences} Occurrences',
+            shape='none',
+            fontcolor='#2c8f82',
+            fontsize=fontsize,
+            penwidth='0')
+
+        sorted_prev, others = node.prev_and_others(occurrence_threshold)
+        for child in sorted_prev:
+            graph.edge(word, child, dir='back')
+            explore.put(child)
+
+        cached_others[word] = others
+        cached_sorted_prev_count[word] = len(sorted_prev)
+    
+    # Need to draw edges to "other" nodes after the fact so that we can
+    # properly draw the ones to nodes that are already in the graph
+    for word in visited:
+        other_count = len(cached_others[word])
+        for other in cached_others[word]:
+            if other in visited:
+                graph.edge(word, other, dir='back')
+                other_count -= 1
+
+        if cached_sorted_prev_count[word] > 0 and other_count > 0:
+            label = 'And 1 other...' if others == 1 else f'And {len(others)} others...'
+            graph.node(
+                f'{word}_others',
+                label=label,
+                shape='none',
+                fontcolor='#2c8f82',
+                fontsize='8',
+                penwidth='0')
+            graph.edge(word, f'{word}_others', dir='back')
         
-        filtered_children.append((child_word, child_count))
-    
-    for child_word, count in filtered_children:
-        graph.edge(word, child_word)
-        explore.put(child_word)
-    
-    # Only add other node if we have at least one other child
-    if len(filtered_children) > 1 and other_count > 0:
-        id = f'{word}_other'
-        graph.node(id, label=f'{other_count} Other Words')
-        graph.edge(word, id)
+graph_roots(['FORMALITIES', 'ORIENTALISTS'], 80)
+graph_roots(['PHRENOLOGIES'], 20)
 
-    graph.node(word, label=f'{word}\n{node.occurrences} Occurrences')
-
-graph.render(filename='flowchart', format='pdf', cleanup=True)
+graph.render(filename='flowchart', format='png', cleanup=True)
 
